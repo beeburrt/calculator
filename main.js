48bc5d91e8c41342f7850773710596f3030771dc
Brian Burt <beeburrt@outlook.com>
2022-07-08 15:31:00 -0700

    Still working on it

--- main.js
+++ main.js
@@ -1,21 +1,61 @@
-const topDisplay = document.querySelector(".upper-display");
-topDisplay.textContent = "fuck you ken";
-const display = document.querySelector(".main-display");
-display.textContent = "fuck you ken";
+const topDisplay = document.querySelector(".top-display");
+const mainDisplay = document.querySelector(".main-display");
 
-const buttons = document.querySelectorAll("button");
-console.log(buttons);
+let memory = [];
 
-buttons.forEach((button) => {
+let firstValue;
+let nextValue;
+let result;
+
+topDisplay.textContent = "";
+mainDisplay.textContent = "";
+
+const clearBtn = document.querySelector(".clear");
+
+const deleteBtn = document.querySelector(".delete");
+
+const numberBtns = Array.from(document.getElementsByClassName("number"));
+const operatorBtns = Array.from(document.getElementsByClassName("operator"));
+
+console.log(numberBtns);
+
+clearBtn.addEventListener("click", clear);
+
+deleteBtn.addEventListener("click", back);
+
+numberBtns.forEach((button) => {
   button.addEventListener("click", (e) => {
-    const btnClass = e.target.getAttribute("class");
-    if (btnClass.includes("clear")) {
-      clear();
-    }
+    memory.push(button.innerText);
+    // updateDisplay(button);
+    console.log(memory);
+  });
+});
+
+operatorBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    updateDisplay(button);
   });
 });
 
 function clear(e) {
   topDisplay.textContent = "";
-  display.textContent = "";
+  mainDisplay.textContent = "";
+}
+
+function back(e) {
+  memory.pop();
+  console.log(memory);
+}
+
+function updateDisplay(button) {
+  topDisplay.textContent = memory.values;
+  mainDisplay.innerText += button.innerText;
+}
+
+function calculate() {
+  // if (operator === "+") {
+
+  // }
+
+  mainDisplay.innerText = result;
 }
