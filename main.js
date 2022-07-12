1c24105ff7fba2d4c1b60c2f93f188f3f97bc7a4
Brian Burt <beeburrt@outlook.com>
2022-07-12 02:02:03 -0700

    Improve functionality

--- main.js
+++ main.js
@@ -7,53 +7,93 @@ clearBtn.addEventListener("click", clear);
 const deleteBtn = document.querySelector(".delete");
 deleteBtn.addEventListener("click", back);
 
-const numberBtns = Array.from(document.getElementsByClassName("number"));
-const operatorBtns = Array.from(document.getElementsByClassName("operator"));
+const operators = ["÷", "×", "−", "=", "+", "."];
 
-let currentValue;
-let previousValue;
-let operator;
+let currentValue = "";
+let previousValue = "";
+let operator = "";
 let result;
-let memArray = [];
+let historyArray = [];
 
 topDisplay.textContent = "";
-mainDisplay.textContent = "";
+mainDisplay.textContent = "0";
+
+const numberBtns = Array.from(document.getElementsByClassName("number"));
+const dotBtn = document.querySelector(".dot");
 
 numberBtns.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-    mainDisplay.innerText += button.innerText;
-    memArray.push(button.innerText);
-    previousValue = mainDisplay.innerText;
-    console.log(memArray);
+  button.addEventListener("click", () => {
+    const { innerText: btnValue } = button;
+    if (mainDisplay.textContent == "0" && btnValue != ".") {
+      mainDisplay.textContent = " ";
+    }
+    if (!(btnValue == ".") || !historyArray.includes(".")) {
+      mainDisplay.innerText += button.innerText;
+      historyArray.push(button.innerText);
+      currentValue = mainDisplay.innerText;
+      console.log();
+    }
   });
 });
 
+const operatorBtns = Array.from(document.getElementsByClassName("operator"));
+
 operatorBtns.forEach((button) => {
   button.addEventListener("click", (e) => {
-    // updateDisplay(button);
-    console.log(e.target);
+    const { innerText: btnValue } = button;
+    if (
+      historyArray.includes("÷") ||
+      historyArray.includes("×") ||
+      historyArray.includes("−") ||
+      historyArray.includes("=") ||
+      historyArray.includes("+") ||
+      historyArray.includes(".")
+    ) {
+      // button.innerText = "";
+      console.log(btnValue);
+    }
+    if (
+      !(
+        historyArray.includes("÷") ||
+        historyArray.includes("×") ||
+        historyArray.includes("−") ||
+        historyArray.includes("=") ||
+        historyArray.includes("+") ||
+        historyArray.includes(".")
+      )
+    ) {
+      mainDisplay.innerText += button.innerText;
+      historyArray.push(btnValue);
+    }
   });
 });
 
 function clear() {
   topDisplay.textContent = "";
-  mainDisplay.textContent = "";
+  mainDisplay.textContent = "0";
+  historyArray = [];
 }
 
 function back() {
-  memArray.pop();
-  currentValue = memArray.join("");
+  historyArray.pop();
+  currentValue = historyArray.join("");
   console.log(currentValue);
   mainDisplay.innerText = currentValue;
+  if (currentValue == "" || currentValue == ".") {
+    mainDisplay.textContent = "0";
+    historyArray.pop();
+    console.log(historyArray);
+  }
 }
 
 function updateDisplay(button) {
-  mainDisplay.innerText += button.innerText;
+  mainDisplay.textContent += button.innerText;
 }
 
 function calculate() {
-  mainDisplay.innerText = result;
+  currentValue = parseFloat(currentValue);
+  previousValue = parseFloat(previousValue);
+  currentValue = mainDisplay.textContent = result;
 }
 
 function add() {
